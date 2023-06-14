e19b99a2e18efad1b525e1971ff603e6209aebff
jihad <>
2023-06-14 13:44:28 +0700

    recyclerBpartA

---
+++ app/src/main/java/com/abrianus201103648/recyclerviewb/AddMahasiswaMainActivity.java
@@ -0,0 +1,14 @@
+package com.abrianus201103648.recyclerviewb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMahasiswaMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mahasiswa_main);
+    }
+}
